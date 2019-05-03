708c5d567fdc87d6378433d3ec38e9518236a0c2
Deven Gurung <deven9229@gmail.com>
2019-05-03 18:21:00 +0600

    generated new controller called welcome for homepage

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   resources :guides
   resources :rentals
   resources :companies
-  root 'companies#index'
+  root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
