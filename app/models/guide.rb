6c495bc6600c4559e1b6c8862e1dbfa09572ae42
Suman Gurung <suman@casecommons.org>
2019-05-10 09:07:06 +0600

    Save languages for guides in a join table
    And also add seed data for Languages

--- app/models/guide.rb
+++ app/models/guide.rb
@@ -1,2 +1,3 @@
 class Guide < ApplicationRecord
+  has_and_belongs_to_many :languages, join_table: :guide_languages
 end
--- app/models/language.rb
+++ app/models/language.rb
@@ -1,2 +1,3 @@
 class Language < ApplicationRecord
+  has_and_belongs_to_many :guides, join_table: :guide_languages
 end
