d3b7e075c293f5cab1c26955470827597fe2b75b
Deven Gurung <deven9229@gmail.com>
2019-05-29 20:02:07 +0600

    removed validation from rental

--- app/models/rental.rb
+++ app/models/rental.rb
@@ -1,5 +1,3 @@
 class Rental < ApplicationRecord
-  validates :rental_agency_or_person, :contact_number, :address, :presence => true
-  validates :rental_agency_or_person, :uniqueness => true
-
+  
 end
